780e4c534a57c43f1c927cf61b5591763b674408
daihn <1251563140@qq.com>
2018-02-28 20:56:37 +0800

    字符集代码

--- code/字符集代码/CharacterForMFC/mfc.cpp
+++ code/字符集代码/CharacterForMFC/mfc.cpp
@@ -1,5 +1,6 @@
 #include "mfc.h"
 #include <iostream>
+using namespace std;
 MyApp app;
 
 BEGIN_MESSAGE_MAP(MyFrame,CFrameWnd)
@@ -42,7 +43,7 @@ void MyFrame::OnPaint()
 
 	// CString char* String之间转换 char*是桥梁
 	// 1.CString->char *
-	CString str = L"liuxijiao计算机网络";  
+	CString str = L"1.CString->char *";  
 	wchar_t *pWChar = str.GetBuffer(); //获取str的宽字符用数组保存  
 	str.ReleaseBuffer();  
 
@@ -50,26 +51,42 @@ void MyFrame::OnPaint()
 	char *pChar = new char[nLen * 2 + 1];   
 	memset(pChar, 0, nLen * 2 + 1);  
 	int rtnVal = (int)wcstombs(pChar, pWChar, nLen * 2 + 1); //宽字符转换为多字节字符  
+	dc.TextOutW( 100, 100, pChar);
 
 	// 2.char *->string
 	char *ch002 = "2.char *->string";
 	std::string str002 = std::string(ch002);
+	dc.TextOutW( 400, 100, str002.c_str());
 
 	// 3.string->char *
 	std::string str003 = "3.string->char *";
-	char *ch003 = NULL;
+	char *ch003 = new char[100];
 	memset(ch003, 0, 100);
 	strcpy(ch003, str003.c_str());
-	
+	dc.TextOutW( 700, 100, ch003);
+
 	// 4.char *->CString
+	char  *ch004 = "4.char *->CString";
+	CString cstr004 = CString(ch004);
+	dc.TextOutW( 100, 300, cstr004);
 
 	// 5.string->CString
+	std::string str005="5.string->CString";
+	CString cstr005(str005.c_str());//或者CString cstr(str.data());初始化时才行
+	dc.TextOutW( 400, 300, cstr005);
 
 	// 6.CString->string
+	CString cstr006 = L"6.CString->char *";  
+	wchar_t *wch006 = cstr006.GetBuffer(); //获取str的宽字符用数组保存  
+	cstr006.ReleaseBuffer();  
 
+	int nLen006 = cstr006.GetLength(); //获取str的字符数  
+	char *ch006 = new char[nLen006 * 2 + 1];   
+	memset(ch006, 0, nLen006 * 2 + 1);  
+	int rtnVal006 = (int)wcstombs(ch006, wch006, nLen006 * 2 + 1); //宽字符转换为多字节字符
 
-
-
-	dc.TextOutW( 300, 300, ch003);
+	std::string str006 = std::string(ch006);
+	
+	dc.TextOutW( 700, 300, str006.c_str());
 }
 
